d1837047a1aabc3899c43d79eb1291451e834e9c
zbigm <31482563+zbigm@users.noreply.github.com>
2017-09-02 02:59:08 +0800

    Update unit.js

--- src/Scripts/unit.js
+++ src/Scripts/unit.js
@@ -33,7 +33,7 @@ $(function () {
 			}
 		}
 	})   
-	$("body").on("click", ".selected .selected-item", function(){
+	$("body").on("click", ".selected-item span", function(){
 		alert("ok")
 		var text = $(this).parents(".selected-item").find("i").text();
 		for(var j = 0; j < selected.length; j++) {
